185a4991dc02b9bae10180be026f71ce21dfefa3
HouJun11220033 <1144724015@qq.com>
2016-10-14 23:54:09 +0800

    有向无环图
    判读有无回路

---
+++ 题目1448：Legal or Not.cpp
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<vector>
+#include<queue>
+using namespace std;
+vector<int> edge[501];
+queue<int> Q;
+using namespace std;
+int main(){
+	//统计每一个节点的入度
+
+	int inDegree[501];
+	int n,m;
+	while(scanf("%d%d",&n,&m)!=EOF)
+	{
+		if(n==0 && m==0)
+		{
+			break;
+		}
+		//n个节点，入度全部初始化为0
+		//清空每一个节点的"边"信息
+		for(int i=0;i<n;i++)
+		{
+			inDegree[i]=0;
+			edge[i].clear();
+		}
+		while(m--)
+		{
+			int a,b;
+			scanf("%d%d",&a,&b);
+			//a-->b,b的入度+1
+			inDegree[b]++;
+			//第a个容器
+			edge[a].push_back(b);
+		}
+		while(Q.empty()==false)
+		{
+			Q.pop();
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(inDegree[i]==0)
+			{
+				//队列里面存的是入度0信息
+				Q.push(i);
+			}
+		}
+		int cnt=0;
+		while(Q.empty()==false)
+		{
+			int nowP=Q.front();
+			Q.pop();
+			cnt++;
+			for(int i=0;i<edge[nowP].size();i++)
+			{
+				//edge[nowP][i]
+				inDegree[edge[nowP][i]]--;
+				if(inDegree[edge[nowP][i]]==0)
+				{
+					Q.push(edge[nowP][i]);
+				}
+			}
+		}
+		if(cnt==n)
+		{
+			puts("YES");
+		}else
+		{
+			puts("NO");
+		}
+	}
+	
+	return 0;
+}
